4a1dbc9b3c0c33db2124dc210444be21024be9a0
Dhrupal patel <82089866+dhrupalpatidar1313@users.noreply.github.com>
2024-06-19 17:32:13 +0000

    Conversion of datatype

---
+++ Basics/conversationopration.js
@@ -0,0 +1,53 @@
+/*
+let score = 50
+let score2 = "30"
+let score3 = "30sa"
+let bool = true
+
+console.log(typeof score)
+console.log(typeof score2)
+console.log(typeof score3)
+console.log(typeof bool)
+
+let valueinnumber = Number(score)
+let valueinnumber1 = Number(score2)
+let valueinnumber2 = Number(score3)
+let boolconv = Number(bool)
+
+console.log(typeof valueinnumber);
+console.log(typeof valueinnumber1);
+console.log(typeof valueinnumber2);
+console.log(typeof boolconv);
+
+console.log(valueinnumber);
+console.log(valueinnumber1);
+console.log(valueinnumber2);
+console.log(boolconv);
+
+*/
+
+/*
+let islogedIn = 1
+let valid = "d"
+
+console.log(Boolean(islogedIn));
+console.log(Boolean(valid));
+
+*/
+
+
+/*
+1 => true , 0 => false
+"" => false
+"Dhrupal" => true
+any number among 1,0 => true 
+*/
+
+
+
+let number = 33
+
+let strnumber = String(number)
+
+console.log(typeof number);
+console.log(typeof strnumber);
