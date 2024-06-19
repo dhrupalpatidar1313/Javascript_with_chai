7b321c9d7d8a057932e25a520029ff0169d29054
Dhrupal patel <82089866+dhrupalpatidar1313@users.noreply.github.com>
2024-06-19 17:04:19 +0000

    Datatype in javascript

---
+++ Basics/datatypes.js
@@ -0,0 +1,19 @@
+"ues strict"; // treat all js code as a newer version 
+
+alert(3 + 3) // We are using node js (nodejs has different syntax), not browser 
+
+let name = "Dhrupal" // String datatype
+let age = 21 // number datatype
+let islogin = true // Boolean 
+
+/*
+number => 2 to power 53
+BigInt => power 53 to ...
+string => ""
+boolean => true / false 
+null => standalone value (null is type og object in JavaScript)
+undefined => Abhi value assign nahi hui hai
+symbol => unique 
+
+object 
+*/
---
+++ Basics/variable.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "dhrupal@gamil.com"
+var accountPassword = "12345"
+accountCity = "Vadodara"
+
+accountEmail = 'patidar@gmail'
+console.log(accountId);
