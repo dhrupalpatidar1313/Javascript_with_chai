e7f934e11593098a1bdfb10658b4a77cb7d04261
Dhrupal patel <82089866+dhrupalpatidar1313@users.noreply.github.com>
2024-06-21 17:42:17 +0000

    comprehension

---
+++ Basics/04_comprehension.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);  // In this case js convert the null into the 0 or 1 that's why we output is 'True'
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// // === 
+
+// console.log("2" === 2);
